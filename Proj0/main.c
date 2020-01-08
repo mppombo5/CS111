5ff0e667c2a59d765f9eeb8e1ad54d1730b694c2
Matthew Pombo <mppombo5@gmail.com>
2020-01-07 18:14:58 -0800

    First commit for proj 0, rough and untested implementation with no options

---
+++ Proj0/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+const unsigned int BYTE_BUFFER_SIZE = 512;
+
+int main() {
+    // read into a larger buffer so we don't have to make a syscall for EVERY single byte
+    char outbuf[BYTE_BUFFER_SIZE];
+    int charsRead = read(STDIN_FILENO, outbuf, BYTE_BUFFER_SIZE);
+
+    while (charsRead != 0) {
+        // error!
+        if (charsRead == -1) {
+            fprintf(stderr, "Error in reading from standard input.\n%s", strerror(errno));
+            exit(1);
+        }
+        if (write(STDOUT_FILENO, outbuf, charsRead) == -1) {
+            fprintf(stderr, "Error in writing to standard output.\n%s", strerror(errno));
+            exit(1);
+        }
+        charsRead = read(STDIN_FILENO, outbuf, BYTE_BUFFER_SIZE);
+    }
+
+    return 0;
+}
