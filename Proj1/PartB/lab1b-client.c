2f5458d0438d6f57ecc624a301498acc4e576cd2
Matthew Pombo <mppombo5@gmail.com>
2020-01-29 10:29:43 -0800

    First commit for Lab 1B

---
+++ Proj1/PartB/lab1b-client.c
@@ -0,0 +1,11 @@
+/* NAME: Matthew Pombo
+ * EMAIL: mppombo5@gmail.com
+ * ID: 405140036
+ */
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Hello, World!\n");
+    return 0;
+}
